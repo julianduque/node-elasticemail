f8aaf5a9d78a3126db82c632af25262b9f0f2e7c
Craig Hoover <crh3675@gmail.com>
2014-01-19 17:29:34 -0500

    added attachment support

--- elasticemail/api.js
+++ elasticemail/api.js
@@ -11,19 +11,28 @@ Api.prototype.request = function(path, method, data, cb) {
 
   data['username'] = this.username;
   data['api_key'] = this.apiKey;
-
+  
   var options = {
     url: this.endpoint + path,
     method: method
-  };
+  };  
+  
+  if(data.file) {
+   data['file'] = data.file;
+   options.body = data.buffer;
+   
+   delete(data.buffer);
+  }  
 
-  if(method.toLowerCase() === 'post') {
-    options['json'] = data;
-  } else {
-    options['qs'] = data;
-  }
+  options['qs'] = data;
+  
+  console.log(options);
 
   request(options, function(err, res, body) {
+  
+   console.log('RESPONSE');
+   console.log(err);
+  
     cb(err, body);
   });
 
--- elasticemail/modules/mailer.js
+++ elasticemail/modules/mailer.js
@@ -1,4 +1,5 @@
 var xml2js = require('xml2js');
+var async = require('async');
 
 var parser = new xml2js.Parser({
   explicitRoot: false,
@@ -15,7 +16,49 @@ function Mailer(api) {
 }
 
 Mailer.prototype.send = function(msg, cb) {
-  this.api.request('/mailer/send', 'post', msg, cb);
+
+  var self = this;
+
+  if(msg.attachments && Object.prototype.toString.call(msg.attachments) == '[object Array]') {
+    
+    var processes = [];
+    var uploads   = [];
+    
+    msg.attachments.forEach(function(attachment){    
+      
+      (function(name, buffer){
+      
+         processes.push(function(next){
+            
+            self.api.request('/attachments/upload', 'put', { 'file' : name, 'buffer' : buffer }, function(err, body) {            
+              
+               if(body) 
+                  uploads.push(body.replace(/^\s+|\s+$/g,''));
+
+               next(err, uploads);
+       
+            });
+         });
+         
+      })(attachment.name, attachment.buffer);
+    });
+    
+    async.waterfall(processes, function(err,uploads) {
+      
+      if(err){
+         cb(err);
+      }
+      else {       
+         msg.attachments = uploads.join(';');         
+         self.api.request('/mailer/send', 'post', msg, cb);  
+      }    
+    });
+  
+  }
+  else
+  {
+    self.api.request('/mailer/send', 'post', msg, cb);
+  }
 };
 
 Mailer.prototype.status = function(jobId, cb) {
